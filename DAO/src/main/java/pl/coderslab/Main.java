12d56f565693df7a486161f5e62d6059c2ad2cc2
krudnik <krzysztof.rudnik@psi.wroc.pl>
2023-01-05 15:59:54 +0100

    moje DAO pierwszy commit

--- DAO/src/main/java/pl/coderslab/Main.java
+++ DAO/src/main/java/pl/coderslab/Main.java
@@ -1,7 +1,42 @@
 package pl.coderslab;
 
+import pl.coderslab.entity.User;
+import pl.coderslab.entity.UserDao;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // TEST METODY CREATE
+
+//        User user = new User();
+//        user.setUserName("Krzysztof");
+//        user.setEmail("trueberserker@tlen.pl");
+//        user.setPassword("trudne_hasło");
+//        UserDao userDao = new UserDao();
+//        userDao.create(user);
+
+        // TEST METODY READ
+
+//        UserDao userDao = new UserDao();
+//        User read = userDao.read(2);
+//        System.out.println(read);
+
+        // TEST METODY UPDATE
+
+//        UserDao userDao = new UserDao();
+//        User user = userDao.read(1);
+//        user.setUserName("Edyta");
+//        user.setEmail("edduch@gmail.com");
+//        user.setPassword("jeszcze_trudniejsze");
+//        userDao.update(user);
+
+        // TEST METODY DELETE
+
+//        UserDao userDao = new UserDao();
+//        userDao.delete(2);
+
+        // TEST METODY FINDALL
+
+
     }
 }
